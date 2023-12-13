9bb72f7225def139dfe94327958bc210a643a57b
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2023-12-13 08:04:29 +0100

    color generator done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const hex = document.querySelector(".color");
+const button = document.querySelector(".generate");
+
+const generateColor = () => {
+  let generatedColor = Math.random().toString(16).substring(2, 8);
+  document.body.style.backgroundColor = "#" + generateColor;
+  hex.innerHTML = "#" + generateColor;
+};
+button.addEventListener("click", generateColor);
